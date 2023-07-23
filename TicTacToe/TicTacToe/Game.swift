05941480bf99f28b8a618006a627e5dd4893ec20
bict0r <victorfdz@pm.me>
2023-07-23 00:54:13 -0500

    Update Game.swift
    Updated code

--- TicTacToe/TicTacToe/Game.swift
+++ TicTacToe/TicTacToe/Game.swift
@@ -2,7 +2,7 @@
 //  Game.swift
 //  TicTacToe
 //
-//  Created by Russell Collins on 7/12/23.
+//  Created by Victor Fernandez on 7/12/23.
 //
 
 import Foundation
@@ -24,16 +24,16 @@ struct Game {
     static var O = "O"
     static var S = " "
     
-    var turn: String = Game.S
-    var top:    [String] = ["X", "X", "X"]
-    var middle: [String] = ["X", "X", "X"]
-    var bottom: [String] = ["X", "X", "X"]
+    var turn: String = Game.X
+    var board: [[String]] = [[Game.S, Game.S, Game.S],
+                             [Game.S, Game.S, Game.S],
+                             [Game.S, Game.S, Game.S]]
     
     mutating func reset() {
-        for n in 0...2 {
-            top[n] = Game.S
-            middle[n] = Game.S
-            bottom[n] = Game.S
+        for row in 0...2 {
+            for col in 0...2 {
+                board[row][col] = Game.S
+            }
         }
         turn = Game.X
     }
@@ -47,50 +47,23 @@ struct Game {
     }
     
     func getMark(inRow row: Int, andColumn col: Int) -> String {
-        switch row {
-        case 0:     return top[col]             // top row
-        case 1:     return middle[col]          // middle row
-        case 2:     return bottom[col]          // bottom row
-        default:    return Game.S
-        }
+        return board[row][col]
     }
     
     func canPlace(inRow row: Int, andColumn col: Int) -> Bool {
-        switch row {
-        case 0:     return top[col] == Game.S           // top row
-        case 1:     return middle[col] == Game.S        // middle row
-        case 2:     return bottom[col] == Game.S        // bottom row
-        default:    return false
-        }
+        return board[row][col] == Game.S
     }
     
     mutating func place(inRow row: Int, andColumn col: Int) {
         if !canPlace(inRow: row, andColumn: col) { return }
-  
-        switch row {
-        case 0:     top[col]    = turn        // top row
-        case 1:     middle[col] = turn        // middle row
-        case 2:     bottom[col] = turn        // bottom row
-        default:    return
-        }
+        board[row][col] = turn
         toggle()
-        
-        
     }
     
     mutating func place(inRow row: Int, andColumn col: Int, using mark: String) -> Bool {
         if !canPlace(inRow: row, andColumn: col) { return false }
-  
-        switch row {
-        case 0:     top[col]    = mark        // top row
-        case 1:     middle[col] = mark        // middle row
-        case 2:     bottom[col] = mark        // bottom row
-        default:    return false
-        }
-    
+        board[row][col] = mark
         return true
-        
-        
     }
 
     func isAllIn(first : String, second: String, andThird third: String) -> Bool {
@@ -101,31 +74,40 @@ struct Game {
     }
     
     func haveTTT(onRow row: Int, andColumn col: Int) -> TicTacToe {
-        if isAllIn(first: top[0], second: top[1], andThird: top[2]) {
+        if isAllIn(first: board[0][0], second: board[0][1], andThird: board[0][2]) {
             return .TopRow
         }
-        if isAllIn(first: middle[0], second: middle[1], andThird: middle[2]) {
+        if isAllIn(first: board[1][0], second: board[1][1], andThird: board[1][2]) {
             return .MiddleRow
         }
-        if isAllIn(first: bottom[0], second: bottom[1], andThird: bottom[2]) {
+        if isAllIn(first: board[2][0], second: board[2][1], andThird: board[2][2]) {
             return .BottomRow
         }
-        if isAllIn(first: top[0], second: middle[0], andThird: bottom[0]) {
+        if isAllIn(first: board[0][0], second: board[1][0], andThird: board[2][0]) {
             return .LeftColumn
         }
-        if isAllIn(first: top[1], second: middle[1], andThird: bottom[1]) {
+        if isAllIn(first: board[0][1], second: board[1][1], andThird: board[2][1]) {
             return .MiddleColumn
         }
-        if isAllIn(first: top[2], second: middle[2], andThird: bottom[2]) {
+        if isAllIn(first: board[0][2], second: board[1][2], andThird: board[2][2]) {
             return .RightColumn
         }
-        if isAllIn(first: top[0], second: middle[1], andThird: bottom[2]) {
+        if isAllIn(first: board[0][0], second: board[1][1], andThird: board[2][2]) {
             return .BackwardDiagonal
         }
-        if isAllIn(first: top[2], second: middle[1], andThird: bottom[0]) {
+        if isAllIn(first: board[0][2], second: board[1][1], andThird: board[2][0]) {
             return .ForwardDiagonal
         }
         return .None
+    }
+    
+    func isDraw() -> Bool {
+        for row in board {
+            if row.contains(Game.S) {
+                return false
+            }
+        }
+        return true
     }
 }
 
